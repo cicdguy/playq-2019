3754bb7cebeae6e09c133d940e6ddb94ac095a37
Dinakar K <dinakar@dinakars-air.gateway.sonic.net>
2019-04-14 07:31:39 -0700

    bugfix: use the data class

--- autoscaling.tf
+++ autoscaling.tf
@@ -31,12 +31,12 @@ data "aws_subnet_ids" "all" {
 
 # Create an internet gateway to give our subnet access to the outside world
 resource "aws_internet_gateway" "default" {
-  vpc_id = "${aws_vpc.default.id}"
+  vpc_id = "${data.aws_vpc.default.id}"
 }
 
 # Grant the VPC internet access on its main route table
 resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.default.main_route_table_id}"
+  route_table_id         = "${data.aws_vpc.default.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.default.id}"
 }
