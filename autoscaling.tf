b8b3382c73b99021b0c438dde4d7ea6c709d91b7
Dinakar K <dinakar@dinakars-air.gateway.sonic.net>
2019-04-14 07:18:39 -0700

    Add IGW and route table

--- autoscaling.tf
+++ autoscaling.tf
@@ -29,6 +29,18 @@ data "aws_subnet_ids" "all" {
   vpc_id = "${data.aws_vpc.default.id}"
 }
 
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.default.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.default.id}"
+}
+
 # Get all availability zones for the given region
 data "aws_availability_zones" "available" {}
 
